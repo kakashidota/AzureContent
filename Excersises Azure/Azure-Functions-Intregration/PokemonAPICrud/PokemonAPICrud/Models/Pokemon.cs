bcba0276fcf8757f96576d9abbbed301839df1a2
Kakashidota <robin.kamo@live.se>
2025-04-02 21:12:13 +0200

    Removed repo and added

--- Excersises Azure/Azure-Functions-Intregration/PokemonAPICrud/PokemonAPICrud/Models/Pokemon.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PokemonAPICrud.Models
-{
-    public class Pokemon
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}
